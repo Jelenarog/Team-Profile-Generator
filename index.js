83eb44f9bebe2b02976daca19146cfa93e87a23f
Jelenarog <jelena.rogic90@gmail.com>
2022-12-20 18:18:48 -0700

    added employee logic and cards

--- index.js
+++ index.js
@@ -1,23 +1,120 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const htmlFile = require('./src/mockup');
+const indexHtml = require('./src/mockup');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const team = [];
 
-
-// //function to start engineer
-
-// function employeeCheck (intern, engineer) {
-//     if (intern) {
-//     console.log('works');
-//     } else if (engineer) {
-//     return engineer;
-//     } 
-//   }
-
-function init() {
-    console.log("Please build your team.");
+function addIntern(){
     inquirer
     .prompt([
-        //const manager = [
+            {
+                type: "input",
+                message: "What is team Intern's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is team Intern's ID number?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is Intern's email address?",
+                name: "email"
+            },
+                        {
+                type: "input",
+                message: "What is team Intern's School?",
+                name: "school"
+            },
+
+    ])
+    .then((answers) =>{
+
+        const intern= new Intern (answers.name, answers.id, answers.email, answers.school);
+        team.push(intern);
+        console.log("Adding intern")
+        console.log(team)
+        addMembers();
+})}
+
+
+function addEngineer(){
+    inquirer
+    .prompt([
+            {
+                type: "input",
+                message: "What is team Engineer's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is team Engineers's ID number?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is Engineers's email address?",
+                name: "email"
+            },
+                        {
+                type: "input",
+                message: "What is team Engineers's Github?",
+                name: "github"
+            },
+
+    ])
+    .then((answers) =>{
+        const engineer = new Engineer (answers.name, answers.id, answers.email, answers.github);
+        team.push(engineer);
+        console.log("Added engineer")
+        console.log(team)
+        addMembers();
+})}
+
+function addMembers () {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Which type of team member would you like to add?",
+            name: "action",
+            choices: ["Engineer", "Intern",'I do not want to add more employees']
+        },
+        ])
+    
+        .then(({action})=>{
+            console.log(action)
+            if (action === 'Engineer'){
+               addEngineer();
+            
+            }
+            else if (action === 'Intern'){
+                addIntern();
+             }
+             else{
+                console.log("Our final team")
+                console.log(team)
+                const htmlContent = indexHtml(team)
+                fs.writeFile('index.html', htmlContent, (err) =>
+                 err ? console.error(err) : console.log('Success!')
+       );
+
+             }
+
+        });
+    }
+
+// //function to start building team
+function init() {
+   
+    console.log("Please build your team.");
+
+    inquirer
+    .prompt([
             {
                 type: "input",
                 message: "What is team manager's name?",
@@ -30,39 +127,43 @@ function init() {
             },
             {
                 type: "input",
-                message: "What is team manager's email address?",
+                message: "What is manager's email address?",
                 name: "email"
             },
-            {
+                        {
                 type: "input",
                 message: "What is team manager's office number?",
-                name: "office"
+                name: "officeNumber"
             },
-        
-// //const employeeSelection = [
-    
 
-    {
-        type: "list",
-        message: "Which type of team member would you like to add?",
-        name: "teamMember",
-        choices:["Engineer", "Intern"],
-        validate: (answer) => {
-            if (answer === "Yes") {
-              return true;
-            }
-          },
-    },
-   ]
-   )
+    ])
+
    .then((answers) =>{
-   // const htmlContent = indexHtml(answers)
-    const htmlContent = htmlFile.indexHtml(answers)
-    fs.writeFile('index.html', htmlContent, (err) =>
-    err ? console.error(err) : console.log('Success!')
-       );
-    }
-    );
+       const manager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber);
+    team.push(manager);
 
+   console.log(team);
+    addMembers();
+//    inquirer
+//     .prompt([
+//             {
+//                 type: "list",
+//                 message: "Which type of team member would you like to add?",
+//                 name: "employee",
+//                 choices: ["Manager","Engineer", "Intern",'i do not want to add more employees']
+//             },
+//         ])
+//         .then((answers) =>{
+//             console.log(answers);
+//         })
+
+    // const htmlContent = htmlFile.indexHtml(answers)
+    // fs.writeFile('index.html', htmlContent, (err) =>
+    // err ? console.error(err) : console.log('Success!')
+    //    );
+    }
+   
+    );
+   
 }
 init();
--- src/mockup.js
+++ src/mockup.js
@@ -1,70 +1,102 @@
+const createManagerCard = (manager) => {
+  return`
+  <div class="col-sm-4">
+            <div class="card bg-dark text-light " style="width: 16rem;">
+              <div class="card-body">
+              <h5 class="card-title ">${manager.name}</h5>
+              <p class="card-text">${manager.getRole()}</p>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${manager.id}</li>
+                <li class="list-group-item">Email: <a href='#'>${
+                  manager.email
+                }</a></li>
+                <li class="list-group-item">Office number: ${
+                  manager.officeNumber
+                }</li>
+              </ul>
+            </div>
+         </div>`;
+ 
+};
+
+const createEngineerCard = (engineer) =>{
+return ` <div class="col-sm-4">
+<div class="card bg-dark text-light" style="width: 16rem;">
+  <div class="card-body">
+    <h5 class="card-title ">${engineer.name}</h5>
+    <p class="card-text">${engineer.getRole()}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID:${engineer.id}</li>
+    <li class="list-group-item">Email:${engineer.email}</li>
+    <li class="list-group-item">Github:${engineer.github}</li>
+  </ul>
+</div>
+</div>`
+
+};
+const createInternCard = (intern) =>{
+  return ` <div class="col-sm-4">
+  <div class="card bg-dark text-light" style="width: 16rem;">
+    <div class="card-body">
+      <h5 class="card-title ">${intern.name}</h5>
+      <p class="card-text">${intern.getRole()}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID:${intern.id}</li>
+      <li class="list-group-item">Email:${intern.email}</li>
+      <li class="list-group-item">School:${intern.school}</li>
+    </ul>
+  </div>
+  </div>`
+  
+  };
 //Function to create HTML file
-const indexHtml = ({name,type,id,email,office}) =>`<!DOCTYPE html>
+const indexHtml = (team) => {
+  console.log("In my indexHTML function")
+  console.log(team);
+  var managerCard = createManagerCard(team[0]);
+  team.shift()
+  console.log(team)
+  employeeCards = ""
+  for (const member of team) {
+    if(member.getRole() === "Engineer"){
+     employeeCards+= createEngineerCard(member)
+    } else{
+      employeeCards += createInternCard(member)
+    }
+    
+  }
+  //return `Hey`
+  return `<!DOCTYPE html>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-    crossorigin="anonymous"
-  />
-
-    <title>My Team</title>
-</head>
-<body>
-    <div class="container-fluid text-center p-5 mb-5 bg-dark text-light">
-    <h1>My Team</h1>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+  
+      <title>My Team</title>
+  </head>
+  <body>
+      <div class="container-fluid text-center p-5 mb-5 bg-dark text-light">
+      <h1>My Team</h1>
       </div>
       <div class="container">
-     <div class="row px-5">
-        <div class="col-sm-4">
-    <div class="card bg-dark text-light " style="width: 16rem;">
-        <div class="card-body">
+       <div class="row px-5">
+          ${managerCard}
+          ${employeeCards}
+      </div>
+      </div>
+  </body>
+  </html>`
+};
 
-          <h5 class="card-title ">${name}</h5>
-          <p class="card-text">${type}Type of team member</p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">ID: ${id}</li>
-          <li class="list-group-item">Email: <a href='#'>${email}</a></li>
-          <li class="list-group-item">Office number: ${office}</li>
-        </ul>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="card bg-dark text-light" style="width: 16rem;">
-        <div class="card-body">
-          <h5 class="card-title ">Team member name</h5>
-          <p class="card-text">Type of team member</p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">ID:</li>
-          <li class="list-group-item">Email:</li>
-          <li class="list-group-item">School:</li>
-        </ul>
-      </div>
-    </div>
-     <div class="col-sm-4">
-      <div class="card bg-dark text-light" style="width: 16rem;">
-        <div class="card-body">
-          <h5 class="card-title ">Team member name</h5>
-          <p class="card-text">Type of team member</p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">ID:</li>
-          <li class="list-group-item">Email:</li>
-          <li class="list-group-item">School:</li>
-        </ul>
-  </div>
-    </div>
-    </div>
-</body>
-</html>`
-module.exports = {
-    indexHtml
-  };
-  
+module.exports = indexHtml;
